8329b7436cdc27e6eac5eed727c98bcb385c8937
Yu Feng <yfeng1@waterfall.dyn.berkeley.edu>
2015-07-29 17:27:03 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -5,24 +5,12 @@ setup(name="sharedmem", version="0.2",
       author_email="yfeng1@andrew.cmu.edu",
       description="Easy routines for coding on sharedmem machines",
       url="http://github.com/rainwoodman/sharedmem",
-      download_url="http://web.phys.cmu.edu/~yfeng1/gaepsi/sharedmem-0.2.tar.gz",
       zip_safe=False,
       package_dir = {'sharedmem': 'sharedmem'},
       packages = [
-        'sharedmem', 'sharedmem.core' #, 'sharedmem.lib'
+        'sharedmem'
       ],
       requires=['numpy'],
       install_requires=['numpy'],
-#      ext_modules = [
-#        Extension('sharedmem.' + name, 
-#             [ 'sharedmem/' + name.replace('.', '/') + '.c',],
-#             extra_compile_args=['-O3'],
-#             libraries=[],
-#             include_dirs=[get_include()],
-#             depends = extra
-#        ) for name, extra in [
-#         ('lib._mergesort', []),
-        #]
-#      ]
 )
 
