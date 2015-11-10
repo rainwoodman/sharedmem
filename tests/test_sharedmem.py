485c8aff30fe5a1edc8c09c363aa6543dbcbd312
Yu Feng <rainwoodman@gmail.com>
2015-11-09 23:07:29 -0800

    Add tests for scalar shm array and an example integration.

--- tests/test_sharedmem.py
+++ tests/test_sharedmem.py
@@ -186,6 +186,64 @@ def test_critical():
             return 
     raise AssertionError("Shall not reach here.")
 
+def test_scalar():
+    s = sharedmem.empty((), dtype='f8')
+    s[...] = 1.0
+    assert_equal(s, 1.0)
+
+    with sharedmem.MapReduce() as pool:
+        def work(i):
+            with pool.ordered:
+                s[...] = i
+        pool.map(work, range(10))
+
+    assert_equal(s, 9)
+
+def test_sum():
+    """ 
+        Integrate [0, ... 1.0) with rectangle rule. 
+        Compare results from 
+        1. direct sum of 'xdx' (filled by subprocesses)
+        2. 'shmsum', cummulated by partial sums on each process
+        3. sum of partial sums from each process.
+
+    """
+    xdx = sharedmem.empty(1024 * 1024 * 128, dtype='f8')
+    shmsum = sharedmem.empty(1, dtype='f8')
+
+    shmsum[:] = 0.0
+
+    with sharedmem.MapReduce() as pool:
+
+        def work(i):
+            s = slice (i, i + chunksize)
+            start, end, step = s.indices(len(xdx))
+
+            dx = 1.0 / len(xdx)
+
+            myxdx = numpy.arange(start, end, step) \
+                    * 1.0 / len(xdx) * dx
+
+            xdx[s] = myxdx
+
+            a = xdx[s].sum(dtype='f8')
+
+            with pool.critical:
+                shmsum[:] += a
+
+            return i, a
+
+        def reduce(i, a):
+            # print('chunk', i, 'done', 'local sum', a)
+            return a
+
+        chunksize = 1024 * 1024
+
+        r = pool.map(work, range(0, len(xdx), chunksize), reduce=reduce)
+
+    assert_almost_equal(numpy.sum(r, dtype='f8'), shmsum[0])
+    assert_almost_equal(numpy.sum(xdx, dtype='f8'), shmsum[0])
+
 if __name__ == "__main__":
     import sys
     run_module_suite()
