9981e6387e54da0b2345f91b552ff411809737e2
Yu Feng <rainwoodman@gmail.com>
2020-10-01 08:33:51 -0700

    bump version to 0.3.8

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup
-setup(name="sharedmem", version="0.3.7",
+setup(name="sharedmem", version="0.3.8",
       author="Yu Feng",
       author_email="rainwoodman@gmail.com",
       description="Dispatch your trivially parallizable jobs with sharedmem. ",
